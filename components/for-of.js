d5d47fb5fd45413ae0cd580580e7b68d63b8de4d
moot <oriondo.abp@gmail.com>
2021-07-14 18:51:31 -0400

    Add for-of component

---
+++ components/for-of.js
@@ -0,0 +1,9 @@
+let subreddits = ["soccer", "popheads", "cringe", "books"];
+for (let sub of subreddits) {
+  console.log(sub);
+}
+
+for (let char of "cockadoodledoo") {
+  console.log(char.toUpperCase());
+}
+
