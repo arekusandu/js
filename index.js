faec8239bcdb724eaa596eeab6ec312c16db42cf
moot <oriondo.abp@gmail.com>
2021-07-14 18:26:52 -0400

    While loop

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ console.log(nums, otherNums);
 console.log(`nums.push(9)`);
 nums.push(9);
 console.log(nums, otherNums);
+
+//Nested Multi Dimensional Arrays
+const animalPairs = [
+  ["doe", "buck"],
+  ["ewe", "ram"],
+  ["peahen", "peacock"],
+];
+
+const target = Math.floor(Math.random() * 10);
+let guess = null;
+while (guess !== target) {
+  guess = Math.floor(Math.random() * 10);
+  console.log(`Target: ${target}, Guess: ${guess}`);
+}
+console.log("Congrats! You win the internet!");
